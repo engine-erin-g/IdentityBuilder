b44a4a76bbb600f14b724fc5fa9087e622e35c7c
engine-erin-g <denergea@gmail.com>
2025-10-08 20:19:10 -0400

    Initial Commit

---
+++ identitybuilderTests/identitybuilderTests.swift
@@ -0,0 +1,17 @@
+//
+//  identitybuilderTests.swift
+//  identitybuilderTests
+//
+//  Created by ndrios on 10/8/25.
+//
+
+import Testing
+@testable import identitybuilder
+
+struct identitybuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
